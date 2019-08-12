f6f0fba9ec3ef7c52350c95b2887d19989ce92e0
Tomasz Michalik <michalik@cs.uchicago.edu>
2019-08-12 02:07:12 -0500

    authentication done

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -8,6 +8,7 @@ import About from './components/About';
 import NavBar from './components/NavBar';
 import Form from './components/Form';
 import Logout from './components/Logout';
+import UserStatus from './components/UserStatus';
 
 class App extends Component {
    constructor() {
@@ -98,7 +99,9 @@ class App extends Component {
    render() {
     return (
       <div>
-        <NavBar title={this.state.title} />
+        <NavBar title={this.state.title}
+          isAuthenticated={this.state.isAuthenticated}
+        />
         <section className="section">
           <div className="container">
             <div className="columns">
@@ -120,6 +123,11 @@ class App extends Component {
                     </div>
                   )} />
                   <Route exact path='/about' component={About}/>
+                  <Route exact path='/status' render{() => (
+                    <UserStatus
+                      isAuthenticated={this.state.isAuthenticated}
+                    />
+                  )} />
                   <Route exact path='/register' render={() => (
                     <Form
                       formType={'Register'}
